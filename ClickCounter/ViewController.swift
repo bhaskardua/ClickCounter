388735e80a121aecb7d173407d59125784e9cc68
bhaskardua <bhaskar_dua@yahoo.com>
2016-04-04 03:18:47 +0800

    Added decrement button. Fixed swift compiler suggestions.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //label
-        var label = UILabel()
+        let label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
         
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         self.label=label
         
         //label2
-        var label2 = UILabel()
+        let label2 = UILabel()
         label2.frame = CGRectMake(210, 150, 60, 60)
         label2.text = "0"
         
@@ -35,18 +35,35 @@ class ViewController: UIViewController {
         
         //button
         
-        var button = UIButton()
+        let button = UIButton()
         button.frame = CGRectMake(150, 250, 60, 60)
         button.setTitle("Click", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.view.addSubview(button)
         
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //decrementButton
+        
+        let decrementButton = UIButton()
+        decrementButton.frame = CGRectMake(250, 250, 100, 60)
+        decrementButton.setTitle("Decrement", forState: .Normal)
+        decrementButton.setTitleColor(UIColor.redColor(), forState: .Normal)
+        self.view.addSubview(decrementButton)
+        
+        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     func incrementCount(){
         self.count += 1
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+    
+    func decrementCount(){
+        self.count -= 1
         self.label.text = "\(self.count)"
         self.label2.text = "\(self.count)"
     }
